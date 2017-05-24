406fa959d3ecca4146d5374d22776c046ccecdcf
zhenbao.zhou <zhenbao.zhou@jimu.com>
2017-05-24 14:56:59 +0800

    temp

--- elastic-job-common/elastic-job-common-core/src/main/java/com/dangdang/ddframe/job/event/jmonitor/JobEventJmonitorConfiguration.java
+++ elastic-job-common/elastic-job-common-core/src/main/java/com/dangdang/ddframe/job/event/jmonitor/JobEventJmonitorConfiguration.java
@@ -37,6 +37,7 @@ import java.sql.SQLException;
 public final class JobEventJMonitorConfiguration extends JobEventJMonitorIdentity implements JobEventConfiguration, Serializable {
     
     private static final long serialVersionUID = 3344410699286435226L;
+
     
     @Override
     public JobEventListener createJobEventListener() throws JobEventListenerConfigurationException {
